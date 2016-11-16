6185b6ff4e4a74d41e8a0551fd00bd1b62699f1c
Cesar Gutierrez Tineo <cesar@tineo.mobi>
2016-11-15 21:45:38 -0500

    limpiando clase spring boot

--- src/main/java/com/horarios/DemoApplication.java
+++ src/main/java/com/horarios/DemoApplication.java
@@ -12,64 +12,6 @@ import java.util.Date;
 @Controller
 @SpringBootApplication
 public class DemoApplication {
-
-	@RequestMapping("/")
-	@ResponseBody
-	String home() {
-		return "<input type='submit' value='meow' /><p>c</p><p>d</p><b><span>cesar</span><span>b</span></b>";
-	}
-
-	@RequestMapping("/test")
-	@ResponseBody
-	String test() {
-		return "<input type='submit' value='meow' /><p>c</p><p>d</p><b><span>test 2</span><span>b</span></b>";
-	}
-
-	@RequestMapping(value="/cosa", method=RequestMethod.GET)
-	@ResponseBody
-	ArrayList<Elemento> porget(@RequestParam(value="p", defaultValue = "no hay nada") String param ) {
-		ArrayList<Elemento> lista = new ArrayList<>();
-		for( int i = 0; i < param.length(); i++ ){
-			lista.add(new Elemento(i, String.valueOf( param.charAt(i) )));
-		}
-		return lista;
-	}
-
-	@RequestMapping(value="/cosa", method=RequestMethod.POST)
-	@ResponseBody
-	String otro() {
-		return "Hola cosa post!";
-	}
-
-	@RequestMapping(value="/login", method=RequestMethod.GET)
-	@ResponseBody
-	public ModelAndView login(ModelAndView mv) {
-		//mv.addObject("currentDate", new Date());
-		//mv.addObject("writers", writer.getWriters());
-
-		mv.setViewName("login");
-		return mv;
-	}
-
-	@RequestMapping(value="/master", method=RequestMethod.GET)
-	@ResponseBody
-	public ModelAndView master(ModelAndView mv) {
-		//mv.addObject("currentDate", new Date());
-		//mv.addObject("writers", writer.getWriters());
-
-		mv.setViewName("master");
-		return mv;
-	}
-
-	@RequestMapping(value="/aulas", method=RequestMethod.GET)
-	@ResponseBody
-	public ModelAndView aulas(ModelAndView mv) {
-		//mv.addObject("currentDate", new Date());
-		//mv.addObject("writers", writer.getWriters());
-
-		mv.setViewName("aulas");
-		return mv;
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
