0a90f8efdccf9dee1b222d6c972a8aec804f4f4a
DebraChacaliaza <debra.chacaliaza@unmsm.edu.pe>
2016-11-17 19:50:58 -0500

    usuario base de datos *UsuarioDao :,v

---
+++ src/main/java/com/horarios/model/Disponibilidad.java
@@ -0,0 +1,96 @@
+package com.horarios.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Table (name="disponibilida")
+public class Disponibilidad {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private int iddisponibilidad;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "iddocente")
+    private int iddocente;
+
+    @NotNull
+    private String horainicio;
+    @NotNull
+    private String horafinal;
+    @NotNull
+    private String dia;
+    @NotNull
+    private String ciclo;
+
+    public Disponibilidad() {
+    }
+    public Disponibilidad(int id) {
+        this.iddocente = id;
+    }
+
+    public Disponibilidad(int iddocente, String horainicio, String horafinal, String dia, String ciclo) {
+        this.iddocente = iddocente;
+        this.horainicio = horainicio;
+        this.horafinal = horafinal;
+        this.dia = dia;
+        this.ciclo = ciclo;
+    }
+
+    public int getIddisponibilidad() {
+        return iddisponibilidad;
+    }
+
+    public void setIddisponibilidad(int iddisponibilidad) {
+        this.iddisponibilidad = iddisponibilidad;
+    }
+
+    public int getIddocente() {
+        return iddocente;
+    }
+
+    public void setIddocente(int iddocente) {
+        this.iddocente = iddocente;
+    }
+
+    public String getHorainicio() {
+        return horainicio;
+    }
+
+    public void setHorainicio(String horainicio) {
+        this.horainicio = horainicio;
+    }
+
+    public String getHorafinal() {
+        return horafinal;
+    }
+
+    public void setHorafinal(String horafinal) {
+        this.horafinal = horafinal;
+    }
+
+    public String getDia() {
+        return dia;
+    }
+
+    public void setDia(String dia) {
+        this.dia = dia;
+    }
+
+    public String getCiclo() {
+        return ciclo;
+    }
+
+    public void setCiclo(String ciclo) {
+        this.ciclo = ciclo;
+    }
+}
---
+++ src/main/java/com/horarios/model/DocenteDao.java
@@ -0,0 +1,7 @@
+package com.horarios.model;
+
+/**
+ * Created by Debra on 17/11/2016.
+ */
+public class DocenteDao {
+}
