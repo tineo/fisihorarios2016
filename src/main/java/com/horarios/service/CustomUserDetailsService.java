703739645ca0bebfab82a0c7eb0a984eef2d49cb
Cesar Gutierrez Tineo <cesar@tineo.mobi>
2016-11-20 13:48:20 -0500

    +CustomUserDetailsService

---
+++ src/main/java/com/horarios/service/CustomUserDetailsService.java
@@ -0,0 +1,50 @@
+package com.horarios.service;
+
+import com.horarios.model.Usuario;
+import com.horarios.model.UsuarioDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service("customUserDetailsService")
+public class CustomUserDetailsService implements UserDetailsService{
+
+    @Autowired
+    private UsuarioDao userService;
+
+    @Transactional(readOnly=true)
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        Usuario user = userService.findByUsername(username);
+        //System.out.println("User : "+user);
+        if(user==null){
+            System.out.println("User not found");
+            throw new UsernameNotFoundException("Username not found");
+        }
+        //user.getState().equals("Active")
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+                true, true, true, true, getGrantedAuthorities(user));
+    }
+
+
+    private List<GrantedAuthority> getGrantedAuthorities(Usuario user){
+        List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+
+        /*for(UserProfile userProfile : user.getUserProfiles()){
+            System.out.println("UserProfile : "+userProfile);
+            authorities.add(new SimpleGrantedAuthority("ROLE_"+userProfile.getType()));
+        }*/
+        authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+        System.out.print("authorities :"+authorities);
+        return authorities;
+    }
+
+}
